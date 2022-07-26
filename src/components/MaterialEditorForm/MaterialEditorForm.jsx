66b69a4b57e1cb9390b14eec2cfbea21c1066f73
Ohleh <oleg.kolischuk@gmail.com>
2022-07-26 13:35:55 +0300

    gl

--- src/components/MaterialEditorForm/MaterialEditorForm.jsx
+++ src/components/MaterialEditorForm/MaterialEditorForm.jsx
@@ -4,13 +4,23 @@ const MaterialEditorForm = () => {
   return (
     <Formik
       initialValues={{ title: '', link: '' }}
-      onSubmit={(xxx = console.log(xxx))}
+      onSubmit={(values, actions) => {
+        console.log(values);
+        actions.resetForm();
+      }}
     >
       <Form>
         <label>
           Description
-          <Field name="title"></Field>
+          <Field name="title" type="text" />
         </label>
+        <br />
+        <label>
+          Link here
+          <Field name="link" type="text" />
+          {/* <Field name="link" type="url"></Field> */}
+        </label>
+        <br />
         <button type="button"> Add material</button>
       </Form>
     </Formik>
